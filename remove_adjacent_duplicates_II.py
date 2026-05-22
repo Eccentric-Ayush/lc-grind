d1dcb2cc216c8858a78d9e4d16479a37bf214d2b
Ayush Kumar Singh <kumarayush88422@gmail.com>
2026-05-22 19:49:31 +0530

    Add Optimal Solution

---
+++ remove_adjacent_duplicates_II.py
@@ -0,0 +1,11 @@
+#Optimal Solution
+def removeDuplicates(s, k):
+    stack = []  # [char, count]
+    for c in s:
+        if stack and stack[-1][0] == c:
+            stack[-1][1] += 1
+            if stack[-1][1] == k:
+                stack.pop()
+        else:
+            stack.append([c, 1])
+    return ''.join(c * n for c, n in stack)
