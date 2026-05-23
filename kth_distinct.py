f75c8eca47203966a9b57c88416adcd5f7c39a12
Ayush Kumar Singh <kumarayush88422@gmail.com>
2026-05-24 00:01:14 +0530

    Correct Indentation

--- kth_distinct.py
+++ kth_distinct.py
@@ -1,12 +1,6 @@
 from collections import Counter
 def kthDistinct(arr, k):
     a1 = Counter(arr)
-    # new = dict(sorted(a1.items()))
-    # lst = []
-    # for i in list(new.keys()):
-    #     lst.append(i)
-    # # lst = lst[::-1]
-    # return lst[k-1] if k<=len(lst) else ""
     lst = []
     for j,i in a1.items():
         if i==1:
