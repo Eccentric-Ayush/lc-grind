b0fac1b4453d436221cbf3caab34742bba6befcc
Eccentric-Ayush <kumarayush88422@gmail.com>
2026-06-04 17:57:16 +0000

    Add Optimal Solution

--- shuffle_the_array.py
+++ shuffle_the_array.py
@@ -1,12 +1,12 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        num1 = nums[:n]
-        num2 = nums[n:]
-        res = []
+#Add Optimal Solution
+def shuffle(self, nums, n):
+    num1 = nums[:n]
+    num2 = nums[n:]
+    res = []
+    
+    # Loop n times to grab one element from each half
+    for i in range(n):
+        res.append(num1[i])
+        res.append(num2[i])
         
-        # Loop n times to grab one element from each half
-        for i in range(n):
-            res.append(num1[i])
-            res.append(num2[i])
-            
-        return res
+    return res
