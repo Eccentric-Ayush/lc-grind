2ea5f7398a0ea89aee3edfd09f1ef70dce78c282
Ayush Kumar Singh <kumarayush88422@gmail.com>
2026-06-07 22:37:35 +0530

    [AlgoCommit] LeetCode/Medium/Restore IP Addresses

---
+++ LeetCode/Medium/Restore IP Addresses/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        n = len(s)
+        result = []
+
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(i+2, n):
+                    p1 = s[:i]
+                    p2 = s[i:j]
+                    p3 = s[j:k]
+                    p4 = s[k:]
+
+                    if self.valid(p1) and self.valid(p2) and self.valid(p3) and self.valid(p4) :
+                        result.append(f"{p1}.{p2}.{p3}.{p4}")
+        
+        return result
+
+    def valid(self, part):
+        if len(part) == 0:
+            return False
+        if len(part) > 1 and part[0] == '0':
+            return False
+    
+    # value check
+        if int(part) > 255:
+            return False
+        
+        return True
